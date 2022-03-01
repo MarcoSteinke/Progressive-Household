25896408075d8992a1d337c8d8f64d7df04fb6b6
maste150hhu <mastremolten2@gmail.com>
2022-03-01 22:03:53 +0100

    initial commit

---
+++ src/main/java/com/steinkeit/progressivehousehold/ProgressivehouseholdApplication.java
@@ -0,0 +1,13 @@
+package com.steinkeit.progressivehousehold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressivehouseholdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressivehouseholdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steinkeit/progressivehousehold/ProgressivehouseholdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steinkeit.progressivehousehold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressivehouseholdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
