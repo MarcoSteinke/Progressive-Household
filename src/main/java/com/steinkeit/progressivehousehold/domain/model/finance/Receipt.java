c46ff3d4b254ba451af297be2179d9f8360cf8c5
maste150hhu <mastremolten2@gmail.com>
2022-03-02 17:12:59 +0100

    update equals in Receipt.java

--- src/main/java/com/steinkeit/progressivehousehold/domain/model/finance/Receipt.java
+++ src/main/java/com/steinkeit/progressivehousehold/domain/model/finance/Receipt.java
@@ -1,6 +1,7 @@
 package com.steinkeit.progressivehousehold.domain.model.finance;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public final class Receipt {
 
@@ -16,6 +17,14 @@ public final class Receipt {
         this.amount = amount;
         this.localDate = localDate;
         this.uploadedImage = uploadedImage;
+    }
+
+    public static Receipt error() {
+        return ReceiptFactory.create(-1L, "", -1.0, null, "");
+    }
+
+    public Boolean hasSomeError() {
+        return this.id.equals(ReceiptId.create(-1L));
     }
 
     public Title getTitle() {
@@ -40,5 +49,29 @@ public final class Receipt {
 
     public UploadedImage getUploadedImage() {
         return uploadedImage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Receipt receipt = (Receipt) o;
+
+        if (!id.equals(receipt.id)) return false;
+        if (!title.equals(receipt.title)) return false;
+        if (!amount.equals(receipt.amount)) return false;
+        if (!localDate.equals(receipt.localDate)) return false;
+        return Objects.equals(uploadedImage, receipt.uploadedImage);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + amount.hashCode();
+        result = 31 * result + localDate.hashCode();
+        result = 31 * result + (uploadedImage != null ? uploadedImage.hashCode() : 0);
+        return result;
     }
 }
