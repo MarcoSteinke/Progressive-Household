b6246e9bc2f096e1eb49a449eaa4c41e9aa9b0ca
maste150hhu <mastremolten2@gmail.com>
2022-03-02 19:36:19 +0100

    add UserDTO.java

--- src/main/java/com/steinkeit/progressivehousehold/infrastructure/persistence/dto/UserDTO.java
+++ src/main/java/com/steinkeit/progressivehousehold/infrastructure/persistence/dto/UserDTO.java
@@ -1,3 +1,26 @@
 package com.steinkeit.progressivehousehold.infrastructure.persistence.dto;
 
-public record UserDTO(String username, String password, String email){}
+public class UserDTO{
+
+    private final String username;
+    private final String password;
+    private final String email;
+
+    public UserDTO(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
